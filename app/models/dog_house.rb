967d3418dfa9bf2b476f4bbe5fbf6e2129412fad
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-12-04 15:01:32 +0300

    Deleting associated Data

--- app/models/dog_house.rb
+++ app/models/dog_house.rb
@@ -1,3 +1,6 @@
 class DogHouse < ApplicationRecord
-  has_many :reviews
+  #Deleting our dog house review using dependent::destroy
+  #This is what lets us easily find all the reveiws associated with a dog house instance by simmply calling .reviews on any instance of the DogHouse class 
+
+  has_many :reviews, dependent: :destroy # The has_many association reference also provide user with additional options to customie its behavior. For Instance  . This tell Active Record to delete all the associated records when the parent record is deleted.
 end
